f2deecd907128e6e21a5f1607fec60bc4df9b686
Rokaya Nasr <rokanasr60@gmail.com>
2024-07-04 20:04:38 +0300

    .

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -80,9 +80,8 @@ function Contactus() {
                         </div>
                     </div>
                 </div>
-                <div className='footer bg-opacity-30 nav-blur md:bg-opacity-30 my-container backdrop-blur-lg bg-white'>
-                    <div className='footer-top'></div>
-                    <div className='flex justify-center items-center'>
+                <div className='footer bg-opacity-30 nav-blur md:bg-opacity-30 backdrop-blur-lg bg-white'>
+                    <div className='flex justify-center text-center items-center'>
                         <p>2024 Nano Technology. All rights reserved ©</p>
                     </div>
                 </div>
